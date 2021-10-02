615a97dd8b470ea079cd869243390f4ff4d29306
Anmol TYAGI <anmol_tyagi@outlook.com>
2021-10-02 12:04:38 +0530

    added comments

--- main.cpp
+++ main.cpp
@@ -9,11 +9,16 @@
 int main()
 {
     LinkedLists *pList = LinkedLists::get_object();
+	
+	// node insertion at beginning
     pList->insert_node_beginning(10);
     pList->insert_node_beginning(20);
     pList->insert_node_beginning(30);
     pList->insert_node_beginning(40);
+	// print the complete list
     pList->print_linked_list();
+	// clear out the list
     pList->clear_linked_list();
+	// print back again
     pList->print_linked_list();
 }
